0a421cc17f144d387641583ea9391921e014b246
Kanstantsin Karpau <karpoffk92@gmail.com>
2021-11-07 23:43:33 +0300

    feat: read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,17 @@
 
 const fs = require ('fs');
-//const path = require ('path');
+const path = require ('path');
 
-fs.readFile('./01-read-file/text.txt', (err, data) => {
+/* fs.readFile('./01-read-file/text.txt', (err, data) => {
     if  (err) {
         console.log(err);
     }
     console.log(data.toString());
     }
-);
+); */
+
+const readStream = fs.createReadStream('./01-read-file/text.txt');
+
+readStream.on('data', (chunk)=>{
+console.log(chunk.toString());
+})
