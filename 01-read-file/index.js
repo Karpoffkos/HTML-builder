2ce8adf9252d5260d2096206401eeecb813a57a9
Kanstantsin Karpau <karpoffk92@gmail.com>
2021-11-07 22:47:35 +0300

    feat: read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@
 const fs = require ('fs');
 //const path = require ('path');
 
-fs.readFile('./text.txt', (err, data) => {
+fs.readFile('./01-read-file/text.txt', (err, data) => {
     if  (err) {
         console.log(err);
     }
