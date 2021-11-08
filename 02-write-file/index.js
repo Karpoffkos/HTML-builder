c0ddda16dfda388a0ba25d77d9b9cc2807683cad
Kanstantsin Karpau <karpoffk92@gmail.com>
2021-11-08 21:21:20 +0300

    feat; 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+/* const fs = require("fs");
 
 const process = require ('process');
 const Emitter = require ('events');
@@ -7,3 +7,25 @@ let writeableStream = fs.createWriteStream("hello.txt");
 writeableStream.write("Нажмите Enter!");
 
 writeableStream.end("Завершение записи");
+
+
+const { stdin, stdout } = process;
+stdin.on('data', data => stdout.write(data));
+ */
+const fs = require("fs");
+const path = require ('path');
+const { stdin, stdout } = process;
+const writeableStream = fs.createWriteStream(path.join(__dirname, "text.txt"));
+stdout.write('Как дела?\n');
+stdin.on('data', data => {
+    
+    data.toString().trim() === 'exit' ? process.exit() : writeableStream.write(data);
+    writeableStream.write('');
+    
+});
+process.on('exit', () => stdout.end('Удачи!'));
+process.on('SIGINT', () => {
+    process.exit();
+});
+  
+ 
